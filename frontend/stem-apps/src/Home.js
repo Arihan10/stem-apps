d90975aa78222b6549b36ac5bf792a400916daf5
FrostDev <frostdev123friday@gmail.com>
2023-10-05 02:25:19 -0400

    tweaks

--- frontend/stem-apps/src/Home.js
+++ frontend/stem-apps/src/Home.js
@@ -1,16 +1,19 @@
+import { useNavigate } from "react-router-dom";
 
 function Home()
 {
+    const nav = useNavigate(0);
+
     return (
         <div className="w-full min-h-screen flex items-center justify-center">
-            <div className="pb-80 flex flex-col items-center space-y-8">
-                <h1 className="font-sg font-bold text-6xl md:text-9xl bg-gradient-to-r from-blue-300 to-teal-300 bg-clip-text text-transparent">
+            <div className="pb-80 flex flex-col items-center">
+                <h1 className="font-sg font-bold text-6xl md:text-9xl text-white">
                     STEM
                 </h1>
-                <h2 className="text-white font-sg text-4xl md:text-5xl">
-                    Club
+                <h2 className="text-white font-sg text-2xl tracking-[0.5em] -mt-3">
+                    CLUB
                 </h2>
-                <button type="button" className="applyBtn text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-lg px-8 py-5 text-center mt-6">
+                <button onClick={() => {nav('auth')}} type="button" className="font-sg text-white bg-gradient-to-br from-rose-600 to-orange-400 focus:ring-2 focus:outline-none transition-all duration-200 font-medium rounded-lg text-lg px-8 py-2 text-center mt-10">
                     APPLY
                 </button>
             </div>
