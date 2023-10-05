7aaefcdb9678aafddc893deac2220eb4a2ee5c4c
Arihan10 <sofwarearihan@gmail.com>
2023-10-05 05:33:31 -0400

    revamped UI, added cool coding effects

--- frontend/stem-apps/src/Home.js
+++ frontend/stem-apps/src/Home.js
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import './index.css'
 
 function Home()
 {
@@ -13,12 +15,51 @@ function Home()
                 <h2 className="text-white font-sg text-2xl tracking-[0.5em] -mt-3">
                     CLUB
                 </h2>
-                <button onClick={() => {nav('auth')}} type="button" className="font-sg text-white bg-gradient-to-br from-rose-600 to-orange-400 focus:ring-2 focus:outline-none transition-all duration-200 font-medium rounded-lg text-lg px-12 py-5 text-center mt-10 animate-pulse">
-                    APPLY
-                </button>
+                <CardEffect/>
             </div>
         </div>
     );
 }
+
+function CardEffect() {
+    const nav = useNavigate(0);
+
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const [randomText, setRandomText] = useState("");
+    
+    const handleOnMove = (e, ref) => {
+      const rect = ref.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+            
+      setRandomText(Array.from(Array(1500)).map(() => chars[Math.floor(Math.random() * (chars.length - 1))]).join(""));
+      
+      const letters = ref.querySelector(".card-letters");
+      letters.style.setProperty("--x", `${x}px`);
+      letters.style.setProperty("--y", `${y}px`);
+    }
+  
+    return (
+      <div className="card-track">
+        <div className="card-wrapper">
+          <div className="card" onMouseMove={(e) => handleOnMove(e, e.currentTarget)}>
+            <div className="card-image">
+                <button onClick={() => {nav('auth')}} type="button" className="font-sg text-white bg-gradient-to-br from-rose-600 to-orange-400 focus:ring-2 focus:outline-none transition-all duration-200 font-medium rounded-lg text-lg px-12 py-5 text-center mt-10">
+                    APPLY
+                </button>
+            </div>
+            <div className="card-gradient"></div>
+            <div className="card-letters">{randomText}</div>
+          </div>
+          <div className="card-corners">
+            <span className="card-corner"></span>
+            <span className="card-corner"></span>
+            <span className="card-corner"></span>
+            <span className="card-corner"></span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 export default Home;
